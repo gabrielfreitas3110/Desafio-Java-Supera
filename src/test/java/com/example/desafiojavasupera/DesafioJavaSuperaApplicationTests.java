161f158cc663e5793c39b1a43cb44315d988896e
gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-10-06 00:03:13 -0300

    Project created

---
+++ src/test/java/com/example/desafiojavasupera/DesafioJavaSuperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiojavasupera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaSuperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
